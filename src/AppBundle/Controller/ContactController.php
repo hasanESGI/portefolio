3163720aa7b932deaf466116ba5b4aae6b07ea49
John Doe <hassan.akar@outlook.fr>
2018-04-09 16:46:21 +0200

    formulaire de contact + heritage de template pour home et cotnact

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -3,9 +3,14 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Contact;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 
 
 
@@ -26,9 +31,35 @@ class ContactController extends Controller
      * @Route("/", name="contact_index")
      * @Method("GET")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('contact/index.html.twig');
+        $contact = new Contact();
+
+
+        $form = $this->createFormBuilder($contact)
+            ->add('name', TextType::class, array('label'=> 'nom'))
+            ->add('email', TextType::class, array('label'=> 'email'))
+            ->add('subject', TextType::class, array('label'=> 'sujet'))
+            ->add('message', TextareaType::class, array('label'=> 'message'))
+            ->add('Save', SubmitType::class, array('label'=> 'envoyer'))
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $contact = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            //return $this->redirectToRoute('task_success'); Doc de symfony
+        }
+        return $this->render('contact/index.html.twig', array(
+            'form' =>$form->createView(),
+        ));
     }
+
+
 
 }
